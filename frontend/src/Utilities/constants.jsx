08307164a3c0c7936107a1f962215f221811f8f0
MatthewBrummund <143558807+MatthewBrummund@users.noreply.github.com>
2025-06-11 16:08:32 -0700

    deletion

--- frontend/src/Utilities/constants.jsx
+++
@@ -1,4 +0,0 @@
-
-
-export const WEBSOCKET_API = ""; // URL for the WebSocket API endpoint NOTE: this doesn't include the route, eg sendMessage
-export const DEBUG = true; // Enable or disable debug mode
